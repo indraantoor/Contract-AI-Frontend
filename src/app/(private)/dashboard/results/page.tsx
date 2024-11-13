825ef743eccf9e9b561dd973aa84b8421723488b
indraantoor <indraantoor@gmail.com>
2024-11-13 20:23:42 +0530

    feat: stripe integration
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/app/(private)/dashboard/results/page.tsx
+++ src/app/(private)/dashboard/results/page.tsx
@@ -1,17 +1,58 @@
 'use client';
 
 import ContractAnalysisResults from '@/components/analysis/ContractAnalysisResults';
+import EmptyState from '@/components/analysis/EmptyState';
+import { useSubscription } from '@/hooks/useSubscription';
+import { api } from '@/lib/api';
+import stripePromise from '@/lib/stripe';
 import { useContractStore } from '@/store/zustand';
+import { toast } from 'sonner';
 
 export default function ContractResultsPage() {
   const analysisResults = useContractStore((state) => state.analysisResults);
 
+  const {
+    subscriptionStatus,
+
+    setLoading,
+  } = useSubscription();
+
+  if (!subscriptionStatus) {
+    return <div>Loading...</div>;
+  }
+
+  const isActive = subscriptionStatus.status === 'active';
+
+  const handleUpgrade = async () => {
+    setLoading(true);
+    if (!isActive) {
+      try {
+        const response = await api.get('/payments/create-checkout-session');
+        const stripe = await stripePromise;
+        await stripe?.redirectToCheckout({
+          sessionId: response.data.sessionId,
+        });
+      } catch (error) {
+        console.error(error);
+        toast.error('Please try again or login to your account');
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      toast.error('You are already a premium member');
+    }
+  };
+
+  if (!analysisResults) {
+    return <EmptyState title="No Analysis" description="Please try again" />;
+  }
+
   return (
     <ContractAnalysisResults
+      contractId={analysisResults._id}
+      isActive={isActive}
       analysisResults={analysisResults}
-      contractId={analysisResults?._id ?? ''}
-      isActive={false}
-      onUpgrade={() => {}}
+      onUpgrade={handleUpgrade}
     />
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { api } from '@/lib/api';
+import stripePromise from '@/lib/stripe';
+
 export default function Home() {
-  return <h1>Helloo</h1>;
+  const handleUpgrade = async () => {
+    try {
+      const response = await api.get('/payments/create-checkout-session');
+      const stripe = await stripePromise;
+      await stripe?.redirectToCheckout({
+        sessionId: response.data.sessionId,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <h1>
+      <Button variant="outline" onClick={handleUpgrade}>
+        Upgrade To Premium
+      </Button>
+    </h1>
+  );
 }
